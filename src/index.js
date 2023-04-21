e4bd174f3f6b068536ea1710124890b5c04911fe
Samuel Spielmann <samuelgspielmann@gmail.com>
2023-04-21 15:51:32 +0200

    try adding some basic page layout

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  return (
-    <div>
-      <h1>Hello, world!</h1>
-    </div>
-  );
+    return (
+        <div className="app-container">
+          <div className="artist-container">
+            <img src="https://via.placeholder.com/150" alt="Artist" />
+            <h1>Artist Name</h1>
+            <p>Artist description goes here</p>
+          </div>
+        </div>
+      );
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
