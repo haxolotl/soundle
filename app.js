9502782ee182d857eeb16b0221db303ac6dd2b85
Samuel Spielmann <samuelgspielmann@gmail.com>
2023-04-21 14:03:01 +0200

    add secrets setup and an outline for api access

---
+++ app.js
@@ -0,0 +1,16 @@
+const axios = require('axios');
+const secrets = require('./secrets');
+
+const API_KEY = secrets.LASTFM_API_KEY;
+
+const searchArtist = async (artistName) => {
+  const url = `http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=${artistName}&api_key=${API_KEY}&format=json`;
+  try {
+    const response = await axios.get(url);
+    console.log(response.data.results.artistmatches.artist);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+searchArtist('Radiohead');
