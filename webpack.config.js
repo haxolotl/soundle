7b9439144ec69d9406fabdb52dc24cd0f773c1bd
Samuel Spielmann <samuelgspielmann@gmail.com>
2023-04-22 15:13:01 +0200

    localhost is kinda working now

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,13 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'), // weird, should be public? not sure how to properly set up public vs dist
+    },
+    compress: true,
+    port: 9000,
+  },
   module: {
     rules: [
       {
@@ -22,6 +29,19 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'images/',
+              publicPath: 'images/'
+            }
+          }
+        ]
+      }
     ]
   }
 };
