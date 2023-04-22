2bea372dcb3299f64b59d8b304e8e11649a1c834
Samuel Spielmann <samuelgspielmann@gmail.com>
2023-04-22 15:27:26 +0200

    extract apiCaller, and trigger it with a button lol

--- src/LastFmApiCaller.js
+++ src/LastFmApiCaller.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+const secrets = require('../secrets');
+
+const API_KEY = secrets.LASTFM_API_KEY;
+const artistName = 'Radiohead';
 
 const LastFmApiCaller = () => {
   const [data, setData] = useState(null);
   
   const handleClick = () => {
-    axios.get(`https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=Cher&api_key=YOUR_API_KEY&format=json`)
-      .then(res => {
+    axios.get(`https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${artistName}&api_key=${API_KEY}&format=json`)
+    .then(res => {
         setData(res.data);
       })
       .catch(err => console.log(err));
